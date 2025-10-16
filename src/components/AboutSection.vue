b0d79099ee77e21d861d7bb3d147f840d2bbfbb2
BrendaQu <brenda.quach@colorado.edu>
2025-10-15 21:13:13 -0600

    Intial Commit

---
+++ src/components/AboutSection.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="about-me">
+    <div class="about-me-text">
+      <div>Hi, I'm Brenda Quach.</div>
+      <div>Software Engineer.</div>
+      <div class="about-summary"> Frontend-focused engineer. I build intuitive interfaces and data visualizations that make complexity simple. I love crafting experiences that feel effortless to use. </div>
+    </div>
+    <div class="about-me-img">
+      <img src="/public/profile-pic.png"></img>
+    </div>
+  </div>
+</template>
+<script lang="ts"></script>
+<style lang="css">
+.about-me {
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  height: 500px;
+  justify-content: center;
+  align-items: center;
+  font-size: 30px;
+}
+.about-summary {
+  width: 400px;
+  font-size: 18px;
+}
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="nav-wrapper">
+    <div class="nav-home">Brenda Quach</div>
+    <ul class="nav nav-pills nav-items-wrapper">
+      <li class="nav-item">
+        <a class="nav-link" href="#"> <i class="fa-solid fa-user" />About Me</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+          <i class="fa-solid fa-laptop" />Experience</a
+        >
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+          <i class="fa-solid fa-laptop" />Projects</a
+        >
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"> <i class="fa-solid fa-laptop" />Skills</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"
+          ><i class="fa-solid fa-envelope" />Contact Me</a
+        >
+      </li>
+    </ul>
+
+    <div class="nav-side-links">
+      <a href="https://github.com/brendaqu" target="_blank"
+        ><i class="fa-brands fa-github" />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/brenda-q-50778617a/"
+        c
+        target="_blank"
+      >
+        <i class="fa-brands fa-linkedin" />
+      </a>
+    </div>
+  </div>
+</template>
+<script lang="ts"></script>
+<style lang="css">
+.nav-wrapper {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 60px;
+}
+
+.nav-home {
+  font-size: 24px;
+  font-weight: bold;
+  padding: 2px;
+  margin-left: 20px;
+}
+
+.nav-side-links {
+  margin-right: 20px;
+}
+
+.nav-side-links a {
+  color: black;
+}
+.nav-items-wrapper {
+  display: flex;
+  font-size: 18px;
+  gap: 10px;
+  font-weight: 500;
+  border: 1px solid #f2f2f2;
+  background-color: #f2f2f2;
+  border-radius: 20px;
+}
+
+.nav-item a {
+  color: gray;
+}
+a:hover {
+  color: black !important;
+}
+
+.fa-solid {
+  margin-right: 5px;
+}
+.fa-brands {
+  font-size: 24px;
+  margin-right: 5px;
+}
+</style>
