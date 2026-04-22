7d34666b2c908ac5338bcbb9d96bd11d09b55330
BrendaQu <brenda.quach@colorado.edu>
2026-04-21 22:49:38 -0600

    Update footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="footer-wrapper">© 2026</div>
+  <div class="footer-wrapper">© 2026 · Designed & Built by Brenda Quach</div>
 </template>
 <style lang="css">
 .footer-wrapper {
   width: 100%;
   height: 30px;
   display: flex;
+  padding: 50px 20px 20px 20px;
   justify-content: center;
   align-items: center;
-  background-color: #a4cb9d;
   color: black;
 }
 </style>
